ab11750d7140dfd7656ab669a453653f1f5ac201
mitroc <michaltrocha@gmail.com>
2017-06-13 00:42:22 +0200

    Transcipted text displayed in one line in window (overwrites)

--- script/script.js
+++ script/script.js
@@ -7,9 +7,18 @@ const recognition = new SpeechRecognition();
 /* SR Settings */
 recognition.interimResults = true;  // Display results on the fly (even non-final).
 recognition.lang = 'pl';
-
-recognition.onresult = event => console.log(event);
-
+recognition.onresult = event => {
+  // Get the transcripted sentence.
+  const sentence = event.results[0][0].transcript;
+  paragraph.textContent = sentence;
+};
 recognition.onend = () => recognition.start();
-
 recognition.start();
+
+/* Create a text field where the transctipt is displayed. */
+let paragraph = document.createElement('p');
+paragraph.className = 'speech-sentence';
+
+const speech = document.querySelector('#speech');
+speech.appendChild(paragraph);
+
