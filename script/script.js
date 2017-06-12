75f49d1687c00daa0c8f9d5c0535e25095901996
mitroc <michaltrocha@gmail.com>
2017-06-12 23:53:55 +0200

    Initiate recognition engine

---
+++ script/script.js
@@ -0,0 +1,15 @@
+/* Browser specific support.*/
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+/* Create a new instance of SR, that will control the recognition for the application. */
+const recognition = new SpeechRecognition();
+
+/* SR Settings */
+recognition.interimResults = true;  // Display results on the fly (even non-final).
+recognition.lang = 'pl'; 
+
+recognition.onresult = event => console.log(event);
+
+
+recognition.start();
+
