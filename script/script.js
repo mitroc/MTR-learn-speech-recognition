1d6d962eedf70f6163b5ab4f3676295fdab035be
mitroc <michaltrocha@gmail.com>
2017-06-13 08:33:28 +0200

    Results displayed in separate paragraphs

--- script/script.js
+++ script/script.js
@@ -7,11 +7,15 @@ const recognition = new SpeechRecognition();
 /* SR Settings */
 recognition.interimResults = true;  // Display results on the fly (even non-final).
 recognition.lang = 'pl';
+
+/* Add event listener fired when service returns a recognised phrase. Unbinds when done. */
 recognition.onresult = event => {
   // Get the transcripted sentence.
   const sentence = event.results[0][0].transcript;
   paragraph.textContent = sentence;
+  appendNewLine(event);
 };
+
 recognition.onend = () => recognition.start();
 recognition.start();
 
@@ -22,3 +26,11 @@ paragraph.className = 'speech-sentence';
 const speech = document.querySelector('#speech');
 speech.appendChild(paragraph);
 
+/* Append new paragraphs when previous sentence is verified. */
+function appendNewLine (event) {
+  if (event.results[0].isFinal) { 
+    paragraph = document.createElement('p');
+    paragraph.className = 'speech-sentence';
+    speech.appendChild(paragraph);
+  } 
+}
